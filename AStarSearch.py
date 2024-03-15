b5fddfe32105a471ccf85ef0b5dd1c8d45cb9dd3
hemmerlinaj20 <100312616+hemmerlinaj20@users.noreply.github.com>
2024-03-15 12:29:01 -0400

    A* search improvements and updates. Still not complete, but does work

--- AStarSearch.py
+++ AStarSearch.py
@@ -5,13 +5,12 @@ from NPuzzle import NPuzzle
 from queue import PriorityQueue
 from dataclasses import dataclass, field
 from typing import Any
-#import copy
 
 @dataclass(order=True)
 class Node:
     priority: int
     puzzle: NPuzzle=field(compare=False)
-    parent: NPuzzle=field(compare=False)
+    parent: Any=field(compare=False)
 
 def in_list(item, list):
     for val in list:
@@ -19,7 +18,24 @@ def in_list(item, list):
             return True
     return False
 
+def generate_successor(successor: NPuzzle, parent: Node, g: int, fringe: PriorityQueue, closed: list, verbosity: int):
+    # Check the closed list for this state
+    if not in_list(successor.state, closed):
+        s_node: Node = Node(successor.manhatten_distance()+g, successor, parent)
+        if verbosity == 2:
+            print(f'Generating successor: {s_node.puzzle.state}')
+            print(f'With a priority of: {s_node.priority}')
+            print()
+        # TODO: check the fringe for this state and value
+        fringe.put(s_node)
+
 def AStarSearch(puzzle: NPuzzle, verbosity: int = 0) -> Node:
+    """Performs A* Search on the given puzzle instance using the manhatten distance heuristic
+
+    With a verbosity greater than 0 prints out extra information
+    Returns . . . (Not sure yet lol)
+    """
+
     h = puzzle.manhatten_distance()
     g = 0
 
@@ -30,6 +46,16 @@ def AStarSearch(puzzle: NPuzzle, verbosity: int = 0) -> Node:
     
         # get the best item in the fringe
         parent: Node = fringe.get()
+
+        if verbosity >= 1:
+            print(f'State removed from fringe: {parent.puzzle.state}')
+            print(f'With a priority of: {parent.priority}')
+            if verbosity == 2:
+                if parent.parent is not None:
+                    print(f'This states parent is: {parent.parent.puzzle.state}')
+                else:
+                    print(f'This state has no parent (start state)')
+            print()
 
         # check if it is the goal
         if parent.puzzle.is_solved:
@@ -47,39 +73,20 @@ def AStarSearch(puzzle: NPuzzle, verbosity: int = 0) -> Node:
             successor: NPuzzle = NPuzzle(parent.puzzle.n, parent.puzzle.state)
 
             if i == 0 and successor.move_up():
-                # Check the closed list for this state
-                if not in_list(successor.state, closed):
-                    s_node: Node = Node(successor.manhatten_distance()+g, successor, parent)
-                    # TODO: check the fringe for this state and value
-                    fringe.put(s_node)
-
-            elif i == 1 and successor.move_right():
-                # Check the closed list for this state
-                if not in_list(successor.state, closed):
-                    s_node: Node = Node(successor.manhatten_distance()+g, successor, parent)
-                    # TODO: check the fringe for this state and value
-                    fringe.put(s_node)
-
-            elif i == 2 and successor.move_down():
-                # Check the closed list for this state
-                if not in_list(successor.state, closed):
-                    s_node: Node = Node(successor.manhatten_distance()+g, successor, parent)
-                    # TODO: check the fringe for this state and value
-                    fringe.put(s_node)
-
-            elif i == 3 and successor.move_left():
-                # Check the closed list for this state
-                if not in_list(successor.state, closed):
-                    s_node: Node = Node(successor.manhatten_distance()+g, successor, parent)
-                    # TODO: check the fringe for this state and value
-                    fringe.put(s_node)
+                generate_successor(successor, parent, g, fringe, closed, verbosity)
+            if i == 1 and successor.move_right():
+                generate_successor(successor, parent, g, fringe, closed, verbosity)
+            if i == 2 and successor.move_down():
+                generate_successor(successor, parent, g, fringe, closed, verbosity)
+            if i == 3 and successor.move_left():
+                generate_successor(successor, parent, g, fringe, closed, verbosity)
 
     return False
-    
+
 
 if __name__ == '__main__':
-    p1 = NPuzzle(8, [[1,2,3],[4,5,6],[0,7,8]])
-    result = AStarSearch(p1)
+    p1 = NPuzzle(8, [[1,2,3],[4,0,5],[7,8,6]])
+    result = AStarSearch(p1, 2)
 
     if result != False:
         print(result.priority)
