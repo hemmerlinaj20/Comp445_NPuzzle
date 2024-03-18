43e38008abfe85820630c715aa1ac42972638d3a
STEIGERNA20 <82662750+STEIGERNA20@users.noreply.github.com>
2024-03-17 22:52:04 -0400

    Update NPuzzle.py
    Added a .copy() to NPuzzle()

--- NPuzzle.py
+++ NPuzzle.py
@@ -4,9 +4,10 @@ import math
 import random
 import copy
 
+
 class NPuzzle:
     """Stores the state and methods of an n-puzzle.
-    
+
     When initialized, creates a randomly generated n-puzzle state
     that is guarenteed to be solvable. May also initialize the puzzle
     with a chosen state specified by the input arguments.*
@@ -23,7 +24,7 @@ class NPuzzle:
 
     def __init__(self, n: int, state: list[list[int]] = None):
         self.n = n
-        if not math.sqrt(n+1).is_integer():
+        if not math.sqrt(n + 1).is_integer():
             raise Exception("n+1 must be a perfect square")
         if state is not None:
             self.state = copy.deepcopy(state)
@@ -34,25 +35,25 @@ class NPuzzle:
     @property
     def goal_state(self) -> list[list[int]]:
         """Returns the goal state of the n-puzzle.
-        
+
         The goal state is defined as all of the numbers are arranged in ascending order
         left to right and top to bottom with the blank space (0) in the bottom right corner.
         """
 
-        base = int(math.sqrt(self.n+1))
+        base = int(math.sqrt(self.n + 1))
         goal_state = []
         for i in range(base):
             goal_state.append([])
         val = 1
         for row in goal_state:
             for i in range(base):
-                if val == self.n+1:
+                if val == self.n + 1:
                     row.append(0)
                 else:
                     row.append(val)
                 val += 1
         return goal_state
-        
+
     @property
     def is_solved(self) -> bool:
         """Returns True if the n-puzzle is in the goal state."""
@@ -61,17 +62,17 @@ class NPuzzle:
             return True
         else:
             return False
-    
+
     def move_up(self) -> bool:
         """Moves the blank space (0) up by one
-        
+
         Returns True if the move succeeds.
         Returns False if the blank space cannot be moved up. For example,
         if the blank space is already at the top of the puzzle.
         """
 
         zero_loc = self.__find_tile__(0)
-        above_zero = (zero_loc[0]-1, zero_loc[1])
+        above_zero = (zero_loc[0] - 1, zero_loc[1])
         # Checks if the blank space is at the top of the puzzle
         if above_zero[0] < 0:
             return False
@@ -80,19 +81,19 @@ class NPuzzle:
         self.state[above_zero[0]][above_zero[1]] = 0
         self.state[zero_loc[0]][zero_loc[1]] = val_above
         return True
-    
+
     def move_down(self) -> bool:
         """Moves the blank space (0) down by one
-        
+
         Returns True if the move succeeds.
         Returns False if the blank space cannot be moved down. For example,
         if the blank space is already at the bottom of the puzzle.
         """
 
         zero_loc = self.__find_tile__(0)
-        below_zero = (zero_loc[0]+1, zero_loc[1])
+        below_zero = (zero_loc[0] + 1, zero_loc[1])
         # Checks if the blank space is at the bottom of the puzzle
-        if below_zero[0] > math.sqrt(self.n+1)-1:
+        if below_zero[0] > math.sqrt(self.n + 1) - 1:
             return False
         # Moves the space
         val_below = self.state[below_zero[0]][below_zero[1]]
@@ -102,14 +103,14 @@ class NPuzzle:
 
     def move_left(self) -> bool:
         """Moves the blank space (0) left by one
-        
+
         Returns True if the move succeeds.
         Returns False if the blank space cannot be moved left. For example,
         if the blank space is already at the far left of the puzzle.
         """
 
         zero_loc = self.__find_tile__(0)
-        left_of_zero = (zero_loc[0], zero_loc[1]-1)
+        left_of_zero = (zero_loc[0], zero_loc[1] - 1)
         # Checks if the blank space is at the far left of the puzzle
         if left_of_zero[1] < 0:
             return False
@@ -121,16 +122,16 @@ class NPuzzle:
 
     def move_right(self) -> bool:
         """Moves the blank space (0) right by one
-        
+
         Returns True if the move succeeds.
         Returns False if the blank space cannot be moved right. For example,
         if the blank space is already at the far right of the puzzle.
         """
 
         zero_loc = self.__find_tile__(0)
-        right_of_zero = (zero_loc[0], zero_loc[1]+1)
+        right_of_zero = (zero_loc[0], zero_loc[1] + 1)
         # Checks if the blank space is at the far right of the puzzle
-        if right_of_zero[1] > math.sqrt(self.n+1)-1:
+        if right_of_zero[1] > math.sqrt(self.n + 1) - 1:
             return False
         # Moves the space
         val_right = self.state[right_of_zero[0]][right_of_zero[1]]
@@ -140,7 +141,7 @@ class NPuzzle:
 
     def __find_tile__(self, tile: int) -> tuple[int, int]:
         """Returns the location of the specified tile in the puzzle.
-        
+
         Location is in the form of (row, column), zero indexed.
         """
 
@@ -148,15 +149,15 @@ class NPuzzle:
             for val in row:
                 if val == tile:
                     return (self.state.index(row), row.index(val))
-                
+
     def __randomize__(self):
         """Randomizes the state of the puzzle.
-        
+
         Randomly performs 1000 moves on the puzzle to randomize the tile positions
         """
 
-        for i in range(0,1000):
-            num = random.randrange(0,1000)/1000
+        for i in range(0, 1000):
+            num = random.randrange(0, 1000) / 1000
             if num < .25:
                 self.move_up()
             elif num < .5:
@@ -168,12 +169,12 @@ class NPuzzle:
 
     def manhatten_distance(self) -> int:
         """Returns the total manhatten distance of the combined tiles
-        
+
         Calculates the manhatten distance of each tile and returns the sum of the distances.
         """
 
         dist = 0
-        for tile in range(1, self.n+1):
+        for tile in range(1, self.n + 1):
             dist += self.__manhatten_helper__(tile)
         return dist
 
@@ -182,12 +183,12 @@ class NPuzzle:
 
         tile_loc = self.__find_tile__(tile)
         dist = abs(tile_loc[0] - self.solved_positions[tile][0]) + abs(tile_loc[1] - self.solved_positions[tile][1])
-        #print(f'Tile: {tile} Dist: {dist}')
+        # print(f'Tile: {tile} Dist: {dist}')
         return dist
-    
+
     def string(self):
         """Returns a string representation of the n-puzzle state"""
-        
+
         s = ""
         for l in self.state:
             s += f'[ '
@@ -195,21 +196,26 @@ class NPuzzle:
                 s += f'{val} '
             s += ']\n'
         return s
-    
+
     @property
-    def solved_positions(self) -> dict[int:tuple[int,int]]:
+    def solved_positions(self) -> dict[int:tuple[int, int]]:
         """The positions of each tile when the puzzle is solved.
-        
+
         A dictionary mapping int (tile) to its solved position (tuple[int, int])
         """
 
         solved_positions = {}
         t = 1
-        for row in range(0, int(math.sqrt(self.n+1))):
-            for col in range(0, int(math.sqrt(self.n+1))):
+        for row in range(0, int(math.sqrt(self.n + 1))):
+            for col in range(0, int(math.sqrt(self.n + 1))):
                 solved_positions[t] = (row, col)
                 t += 1
         return solved_positions
+
+    def copy(self):
+        """Creates a deep copy of the NPuzzle instance."""
+        return NPuzzle(self.n, self.state)
+
 
 if __name__ == '__main__':
     p1 = NPuzzle(8)
