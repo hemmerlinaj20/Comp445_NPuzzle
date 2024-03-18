30a90a924a3e96df23100bc086e4b592655fd465
hemmerlinaj20 <100312616+hemmerlinaj20@users.noreply.github.com>
2024-03-17 21:15:25 -0400

    Created a small demo.py file for the presentation if we need it

---
+++ demo.py
@@ -0,0 +1,23 @@
+from NPuzzle import NPuzzle
+from AStarSearch import AStarSearch, get_path
+
+
+p = NPuzzle(8, [[1,2,3],[4,8,5],[7,0,6]])
+#p = NPuzzle(15, [[1,2,3,4],[5,7,0,8],[9,6,11,12],[13,10,14,15]])
+
+
+print('STARTING STATE:')
+print(p.string())
+
+print('SEARCHING')
+result, runtime, memory = AStarSearch(puzzle = p)
+print('SEARCH COMPLETE')
+
+path = get_path(result)
+print(f'PATH LENGTH: {result.moves}')
+print('PATH:')
+while len(path) > 0:
+    print(path.pop(len(path)-1).string())
+
+print(f'RUNTIME (TOTAL NUMBER OF NODES GENERATED): {runtime}')
+print(f'MEMORY USAGE (PEAK FRINGE SIZE (NUMBER OF NODES)): {memory}')
